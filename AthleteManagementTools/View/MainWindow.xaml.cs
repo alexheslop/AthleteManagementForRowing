177e9ea9925ca81c336f4e2a60c0d14a3833cc91
Alex Heslop <alex.heslop@abd.uk.com>
2018-08-02 15:51:05 +0100

    Add project files.

---
+++ AthleteManagementTools/View/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace AthleteManagementTools.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
