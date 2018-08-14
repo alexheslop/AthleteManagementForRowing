5e4b2cdccc09a1c62b5f6077ea1468367fbe5c58
Alex Heslop <alex.heslop@abd.uk.com>
2018-08-14 17:34:37 +0100

    Added validations although still saying not valid, and changes to wattage percentages etc

--- AthleteManagementTools/Model/ErgTestScorePredictor.cs
+++ AthleteManagementTools/Model/ErgTestScorePredictor.cs
@@ -4,19 +4,37 @@ namespace AthleteManagementTools.Model
 {
     public static class ErgTestScorePredictor
     {
-        public static double Expected5KScore(double Watts2K)
+        public static double Expected5KScore(double watts2K)
         {
-            var score = Watts2K * 0.82;
+            var score = watts2K * 0.82;
             return score;
         }
 
-        public static double Expected30r20Score(double Watts2K)
+        public static double Expected30R20Score(double watts2K)
         {
-            var score = Watts2K*0.73;
+            var score = watts2K*0.7;
             return score;
         }
-        
-        
+
+        public static double ExpectedUt2(double watts2K)
+        {
+            var score = watts2K * 0.55;
+            return score;
+        }
+
+        public static double Expected2Kfrom5KScore(double watts2K)
+        {
+            var score = watts2K / 0.82;
+            return score;
+        }
+
+        public static double Expected2Kfrom30R20Score(double watts2K)
+        {
+            var score = watts2K / 0.7;
+            return score;
+        }
+
+
 
     }
 }
--- AthleteManagementTools/Model/SplitCalculator.cs
+++ AthleteManagementTools/Model/SplitCalculator.cs
@@ -6,35 +6,75 @@ namespace AthleteManagementTools.Model
     public class SplitCalculator
     {
         public readonly ObservableCollection<TrainingZone> TrainingZoneList;
+        public string UT2From2k;
+        public string UT2From5k;
+        public string UT2From3020;
         private readonly double _hrMin;
         private readonly double _hrReserve;
         private readonly string _ut2Split;
         private readonly double _2KWatts;
         
-        public SplitCalculator(double hrMax, double hrMin, string ut2Split)
+        public SplitCalculator(double hrMax, double hrMin, string split, bool isUt2)
         {
-            _ut2Split = ut2Split;
             _hrMin = hrMin;
             _hrReserve = hrMax - _hrMin;
-            var ut2Seconds = TimeToSplitConverter.ConvertSplitToSeconds(ut2Split);
-            _2KWatts = SplitToWattsConverter.SplitToWattsConvert(ut2Seconds)/0.6;
-            TrainingZoneList = new ObservableCollection<TrainingZone> {GetUt2(), GetUt1(), GetAt(), GetTr(), GetAn() };
+            if (isUt2)
+            {
+                _ut2Split = split;
+                var ut2Seconds = TimeToSplitConverter.ConvertSplitToSeconds(split);
+                _2KWatts = SplitToWattsConverter.SplitToWattsConvert(ut2Seconds) / 0.55;
+                TrainingZoneList =
+                    new ObservableCollection<TrainingZone> {GetUt2(), GetUt1(), GetAt(), GetTr(), GetAn()};
+            }
+            else
+            {
+                UT2From2k = GetUT2From2k(split);
+                UT2From5k = GetUT2From5k(split);
+                UT2From3020 = GetUT2From3020(split);
+            }
+            
+            
+            
         }
-        
+
+        private string GetUT2From3020(string split)
+        {
+            var splitsecs = TimeToSplitConverter.ConvertSplitToSeconds(split);
+            var watts = SplitToWattsConverter.SplitToWattsConvert(splitsecs);
+            return TimeToSplitConverter.ConvertSecondsToSplit(
+                SplitToWattsConverter.WattsToSplitConvert(ErgTestScorePredictor.ExpectedUt2(ErgTestScorePredictor.Expected2Kfrom30R20Score(watts))));
+        }
+
+        private string GetUT2From5k(string split)
+        {
+            var splitsecs = TimeToSplitConverter.ConvertSplitToSeconds(split);
+            var watts = SplitToWattsConverter.SplitToWattsConvert(splitsecs/10);
+            return TimeToSplitConverter.ConvertSecondsToSplit(
+                SplitToWattsConverter.WattsToSplitConvert(ErgTestScorePredictor.ExpectedUt2(ErgTestScorePredictor.Expected2Kfrom5KScore(watts))));
+        }
+
+        private string GetUT2From2k(string split)
+        {
+            var splitsecs = TimeToSplitConverter.ConvertSplitToSeconds(split);
+            var watts = SplitToWattsConverter.SplitToWattsConvert(splitsecs / 4);
+            return TimeToSplitConverter.ConvertSecondsToSplit(
+                SplitToWattsConverter.WattsToSplitConvert(ErgTestScorePredictor.ExpectedUt2(watts)));
+        }
+
         private TrainingZone GetUt2()
         {
             var ut2MinHr = Math.Round(_hrMin + (0.6 * _hrReserve), MidpointRounding.AwayFromZero);
-            var ut2MaxHr = Math.Round(_hrMin + (0.7 * _hrReserve), MidpointRounding.AwayFromZero);
-            var ut2Watts = Math.Round(_2KWatts * 0.6, MidpointRounding.AwayFromZero);
+            var ut2MaxHr = Math.Round(_hrMin + (0.75 * _hrReserve), MidpointRounding.AwayFromZero);
+            var ut2Watts = Math.Round(_2KWatts * 0.55, MidpointRounding.AwayFromZero);
             var ut2Zone = new TrainingZone("UT2", ut2Watts, _ut2Split, ut2MinHr, ut2MaxHr);
             return ut2Zone;
         }
 
         private TrainingZone GetUt1()
         {
-            var ut1MinHr = Math.Round(_hrMin + (0.7 * _hrReserve), MidpointRounding.AwayFromZero);
+            var ut1MinHr = Math.Round(_hrMin + (0.75 * _hrReserve), MidpointRounding.AwayFromZero);
             var ut1MaxHr = Math.Round(_hrMin + (0.8 * _hrReserve), MidpointRounding.AwayFromZero);
-            var ut1Watts = Math.Round(_2KWatts * 0.7, MidpointRounding.AwayFromZero);
+            var ut1Watts = Math.Round(_2KWatts * 0.65, MidpointRounding.AwayFromZero);
             var ut1Split = TimeToSplitConverter.ConvertSecondsToSplit(SplitToWattsConverter.WattsToSplitConvert(ut1Watts));
             var ut1Zone = new TrainingZone("UT1", ut1Watts, ut1Split, ut1MinHr, ut1MaxHr);
             return ut1Zone;
@@ -54,7 +94,7 @@ namespace AthleteManagementTools.Model
         {
             var trMinHr = Math.Round(_hrMin + (0.85 * _hrReserve), MidpointRounding.AwayFromZero);
             var trMaxHr = Math.Round(_hrMin + (0.95 * _hrReserve), MidpointRounding.AwayFromZero);
-            var trWatts = Math.Round(_2KWatts * 1.05, MidpointRounding.AwayFromZero);
+            var trWatts = Math.Round(_2KWatts * 0.95, MidpointRounding.AwayFromZero);
             var trSplit = TimeToSplitConverter.ConvertSecondsToSplit(SplitToWattsConverter.WattsToSplitConvert(trWatts));
             var trZone = new TrainingZone("TR", trWatts, trSplit, trMinHr, trMaxHr);
             return trZone;
@@ -81,7 +121,7 @@ namespace AthleteManagementTools.Model
 
         public string Get30R20()
         {
-            var watts30R20 = ErgTestScorePredictor.Expected30r20Score(_2KWatts);
+            var watts30R20 = ErgTestScorePredictor.Expected30R20Score(_2KWatts);
             var target30R20 = TimeToSplitConverter.ConvertSecondsToSplit(SplitToWattsConverter.WattsToSplitConvert(watts30R20));
 
             return target30R20;
