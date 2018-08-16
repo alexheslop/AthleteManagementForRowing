c5f4a90c4d94ab692926427ce50dd0c5f135173c
Alex Heslop <alex.heslop@abd.uk.com>
2018-08-16 17:27:53 +0100

    couple of small changes to validation

--- AthleteManagementTools/ValidationRules/DateValidation.cs
+++ AthleteManagementTools/ValidationRules/DateValidation.cs
@@ -9,7 +9,7 @@ namespace AthleteManagementTools.ValidationRules
         public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
         {
             DateTime time;
-            if (value != null && DateTime.TryParseExact(value.ToString(), "mm:ss.f", cultureInfo, DateTimeStyles.None, out time))
+            if (value != null && DateTime.TryParseExact(value.ToString(), "m:ss.f", cultureInfo, DateTimeStyles.None, out time))
                 return new ValidationResult(true, null);
 
             return new ValidationResult(false, "Please enter a valid string value.");
